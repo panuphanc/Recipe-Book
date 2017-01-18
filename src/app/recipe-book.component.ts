596a7f5e497a95eb65d066a981e2da5782c949b1
panuphanc <nongmee8@gmail.com>
2017-01-18 11:33:30 +0700

    all donw

---
+++ src/app/recipe-book.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes';
+import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
+
+@Component({
+  selector: 'recipe-book-app',
+  templateUrl: './recipe-book.component.html'
+})
+export class RecipeBookAppComponent {
+  
+}
